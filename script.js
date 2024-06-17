3cee8c0eb3edcc0a608bc6e9f790d17d7173e7cb
Harsh-Awasthi <115348870+Harsh-Awasthi@users.noreply.github.com>
2024-06-17 13:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function indexOfIgnoreCase(s1, s2) {
 
     for(let i=0;i<s1.length;i++){
       
-        if(s1.charAt(i)==s3.charAt(0)){
+        if(s1.charAt(i)==s2.charAt(0)){
         break;
         }
         ans++;
